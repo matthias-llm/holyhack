f4475e697574492b52572d46eb6758f2150b7129
toprednax <toprednax@yahoo.com>
2022-03-18 23:43:50 +0100

    dede

--- src/components/tijdens_page.jsx
+++ src/components/tijdens_page.jsx
@@ -4,9 +4,11 @@ import meeting from "../meeting/Meeting_20.mp4";
 export const Tijdens = () => {
   return (
     <div id="main-container" className="min-h-screen min-w-screen">
-      <video>
+      <video width="100%" height="100%" preload="auto">
         <source src={meeting} type="video/mp4" />
       </video>
     </div>
   );
 };
+
+export default Tijdens;
