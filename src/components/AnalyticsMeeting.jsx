3c7eabe2b842f992de8660ae1bfb5994bfbfe937
JDogM <matt.mll@hotmail.com>
2022-03-19 04:51:15 +0100

    Analytics setup

--- src/components/AnalyticsMeeting.jsx
+++ src/components/AnalyticsMeeting.jsx
@@ -1,5 +1,12 @@
 const Analytics = () => {
+	return (
+		<div className="flex flex-col">
+			<div className="flex flex-row">
+				Interactie en weet ik veel wat we nog hebben
+			</div>
 
+		</div>
+	);
 }
 
 export default Analytics;
