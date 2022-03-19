26983180623a06220cea8afdd8f32452e5994cf1
JDogM <matt.mll@hotmail.com>
2022-03-19 14:08:39 +0100

    Analytics

--- src/components/AnalyticsMeeting.jsx
+++ src/components/AnalyticsMeeting.jsx
@@ -1,8 +1,12 @@
+import Image from "../../node_modules/next/image";
+
 const Analytics = () => {
 	return (
 		<div className="flex flex-row divide-x-2 divide-black min-h-screen">
 			<div className="w-1/2 flex flex-col items-center">
 				<p className="bold">Past month</p>
+
+				<Image src={require("../meeting/activity.png")} alt="Lopo"/>
 			</div>
 
 			<div className="w-1/2 flex flex-col items-center">
