68e4f34efb73454f5d6589ff22422809d531187e
toprednax <toprednax@yahoo.com>
2022-03-19 13:33:48 +0100

    m

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -4,21 +4,39 @@ import Transcript from "./NaMeeting";
 import Analytics from "./AnalyticsMeeting";
 
 const StartScreen = () => {
-	return (
-		<div className="flex flex-col">
-			<button onClick={() => ReactDOM.render(<Tijdens />, document.getElementById("root"))}>
-				<p>Videocall</p>
-            </button>
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen min-w-screen">
+      <div className="pb-10">
+        <button
+          className="bg-gray-400 border-4 rounded-lg w-40 h-10"
+          onClick={() =>
+            ReactDOM.render(<Tijdens />, document.getElementById("root"))
+          }
+        >
+          <p>Join Meeting</p>
+        </button>
+      </div>
+      <div className="pb-10">
+        <button
+          className="bg-gray-400 border-4 rounded-lg w-40 h-15"
+          onClick={() =>
+            ReactDOM.render(<Analytics />, document.getElementById("root"))
+          }
+        >
+          <p>Analytics last meeting</p>
+        </button>
+      </div>
 
-			<button onClick={() => ReactDOM.render(<Analytics />, document.getElementById("root"))}>
-				<p>Analytics</p>
-            </button>
-
-			<button onClick={() => ReactDOM.render(<Transcript />, document.getElementById("root"))}>
-				<p>Transcript</p>
-            </button>
-		</div>
-	);
-}
+      <button
+        className="bg-gray-400 border-4 rounded-lg w-40 h-15"
+        onClick={() =>
+          ReactDOM.render(<Transcript />, document.getElementById("root"))
+        }
+      >
+        <p>Transcript last meeting</p>
+      </button>
+    </div>
+  );
+};
 
 export default StartScreen;
--- src/components/TijdensMeeting.jsx
+++ src/components/TijdensMeeting.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import meeting from "../meeting/Meeting_20.mp4";
 import persons from "../meeting/person.png";
 
 import UI_zoom from "../meeting/UI_zoom.png";
 import { BellIcon } from "@heroicons/react/outline";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export const Tijdens = () => {
+  const notification = () =>
+    toast.info("U will be notified when 'Xander' will be mentioned", {
+      className: "text-lg",
+    });
+
+  const suprised = () =>
+    toast.warning("People are looking confused", {
+      className: "text-lg",
+    });
+
+  const [showElement, setSwipe] = useState(true);
+  const onSwipe = () => {
+    setSwipe(!showElement);
+  };
+
   return (
     <div
       id="main-container"
@@ -22,13 +39,18 @@ export const Tijdens = () => {
           <img src={UI_zoom} alt="Logo"></img>
         </div>
       </div>
-      <div className="bg-blue-200 w-1/4 pl-14">
+      <div className="bg-blue-400 w-1/4 pl-14">
+        <div className="absolute">
+          <button className="opacity-0" onClick={onSwipe}>
+            izodjzeiojdoizedj
+          </button>
+        </div>
         <div id="Bullet points div" className="items-center pt-5">
           <p className="text-2xl text-white font-bold">Bullet Points</p>
           <div className="border-4 bg-gray-400 h-96 w-60">
-            <li className="pl-2">Intro</li>
-            <li className="pl-2">Recruiting Process</li>
-            <li className="pl-2">Wanted positions</li>
+            <li className="pl-2 line-through">Intro</li>
+            <li className="pl-2 line-through">Recruiting Process</li>
+            <li className="pl-2 line-through">Wanted positions</li>
             <li className="pl-2">Training and coaching</li>
             <li className="pl-2">Attract new people</li>
             <li className="pl-2">Outro</li>
@@ -37,13 +59,34 @@ export const Tijdens = () => {
         <div id="Noti div" className="pt-5">
           <p className="text-2xl text-white font-bold">Notifications</p>
           <div className="flex flex-row">
-            <input type="text" className="min-w-screen " />
-            <BellIcon stroke="white" className="w-6 h-6" />
+            <input type="text" className="min-w-screen" />
+            <button onClick={notification}>
+              <BellIcon stroke="white" className="w-6 h-6" />
+            </button>
+            <button className="opacity-0" onClick={suprised}>
+              ___
+            </button>
+            <ToastContainer position="top-left" autoClose={8000} />
           </div>
         </div>
         <div id="Deadlines div" className="pt-5">
-          <p className="text-2xl text-white font-bold">Deadlines</p>
-          <div className="border-4 bg-gray-400 h-20 w-60"></div>
+          <div className="flex flex-row">
+            <p className="text-2xl text-white font-bold">Deadlines</p>
+          </div>
+          <div className="border-4 bg-gray-400 h-20 w-60">
+            <div className="flex flex-row pr-2 pl-2">
+              <p className="pr-2">21/03 Finance meeting</p>
+              <button className=" bg-gray-500 text-xs w-10 h-5">Add</button>
+            </div>
+            {showElement ? (
+              <div></div>
+            ) : (
+              <div className="flex flex-row pr-2 pl-2">
+                <p className="pr-2">27/03 Data rapport </p>
+                <button className=" bg-gray-500 text-xs w-10 h-5">Add</button>
+              </div>
+            )}{" "}
+          </div>
         </div>
       </div>
     </div>
